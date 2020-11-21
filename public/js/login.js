c7e8881abe39625336d68a59323f0db60aca5465
jwashi83 <69912215+jwashi83@users.noreply.github.com>
2020-11-20 22:58:31 -0500

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,32 +2,29 @@ $(document).ready(function() {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
   var emailInput = $("input#email");
-  var passwordInput = $("input#password");
+ 
 
-  // When the form is submitted, we validate there's an email and password entered
+  // When the form is submitted, we validate there's an email entered
   loginForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      email: emailInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email) {
       return;
     }
 
-    // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password);
+    // If we have an email we run the loginUser function and clear the form
+    loginUser(userData.email);
     emailInput.val("");
-    passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password) {
-    console.log(email, password)
+  function loginUser(email) {
+    console.log(email)
     $.post("/api/login", {
-      email: email,
-      password: password
+      email: email
     })
       .then(function() {
         window.location.replace("/closet");
